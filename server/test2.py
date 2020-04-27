9330ec705b2809d6acaf8368a49a616dba9a68db
Guennec <64171690+Guennec@users.noreply.github.com>
2020-04-27 17:10:30 +0200

    Start internet site in python

---
+++ server/test2.py
@@ -0,0 +1,8 @@
+#!/Python37/python
+print("Content-type: text/html")
+print("")
+print("<html><head>")
+print("")
+print("</head><body>")
+print("Hello from Python script.")
+print("</body></html>")
